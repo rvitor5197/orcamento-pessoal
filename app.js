256af6a03a993709dc9966de0b4070ecc5350ccd
Vitor Fonseca <rvitor5197@gmail.com>
2021-03-19 14:11:49 -0300

    db class

--- app.js
+++ app.js
@@ -10,6 +10,32 @@ class Despesa{
 	}
 }
 
+class Bd {
+
+	constructor(){
+		let id = localStorage.getItem('id')
+
+		if(id === null){
+			localStorage.setItem('id', 0)
+		} 
+	}
+
+	getProximoId(){
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+
+	gravar(d) {
+		let id = this.getProximoId()
+
+		localStorage.setItem(id, JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
+}
+
+let bd = new Bd()
+
 
 function cadastrarDespesa(){
 
@@ -30,9 +56,6 @@ function cadastrarDespesa(){
 		valor.value
 	)
 
-	gravar(despesa)
+	bd.gravar(despesa)
 }
-
-function gravar(d) {
-	localStorage.setItem('despesa', JSON.stringify(d))
-}
+ 
